76abedb2b57f82e6306431801e566f21c69d746b
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-05-07 15:07:45 +0530

    Modified some stylings...

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -9,7 +9,7 @@ const LoginPage = () => {
         className="h-[97%] w-4/6 bg-no-repeat bg-contain bg-center flex flex-col justify-center items-center"
         style={{ backgroundImage: `url(${logoBg})` }}
       >
-        <div className="w-[35%] bg-[#19516A] rounded-2xl px-5 mb-10 shadow-2xl shadow-zinc-900 flex flex-col justify-center items-center">
+        <div className="w-2/5 bg-[#19516A] rounded-2xl px-5 mb-10 shadow-2xl shadow-zinc-900 flex flex-col justify-center items-center">
           <div className="w-full flex justify-center mt-6">
             <img src={sanjoghelplinelogo} alt="logo" className="w-32 inline" />
             <span className="inline border-[1px] border-zinc-400 mx-2"></span>
@@ -49,7 +49,7 @@ const LoginPage = () => {
         </div>
 
         <div className="text-white text-lg">
-          <button className="bg-[#054161] px-5 py-2 w-72 rounded-md border-2 border-blue-400 active:scale-105 duration-100">
+          <button className="bg-[#054161] px-5 py-2 w-72 rounded-md border-2 border-[#82A0B0] active:scale-105 duration-100">
             Login
           </button>
         </div>
